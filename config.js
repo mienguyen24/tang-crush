b8c04b53d314cb3a79dd853b92f4ff567097f0bd
mienguyen24 <86727450+mienguyen24@users.noreply.github.com>
2021-06-30 20:59:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Chúc eiiiiiiiiiiiiiiiiiiiiiiiiii',
+    introTitle: 'Chúc eiiiiiiiiiiiiiiiiiiiiiiii ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
